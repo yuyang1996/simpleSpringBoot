80cca2dfa7635c9b15dac830f070694d1d35ca1e
weiyuyang <812744337@qq.com>
2021-01-12 14:07:10 +0800

    创建项目

---
+++ src/main/java/com/wyy/simplespringboot/SimplespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wyy.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplespringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyy/simplespringboot/SimplespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyy.simplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
